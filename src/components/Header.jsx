bdfdbba4e34a5d47a46c461da8ad5720af70c9eb
Bernardo Kassick <bernardokassick@gmail.com>
2021-06-10 18:17:25 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header({ children, size }) {
+  let fontSize = size === 'large' ? 'text-5xl' : 'text-xl';
+
+  return (
+    <header>
+      <div className="bg-blue-300 bord  mx-auto p-4">
+        <h1 className={`text-center font-semibold ${fontSize}`}>{children}</h1>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/InvestmentsType.jsx
@@ -0,0 +1,7 @@
+export default function InvestmentsType({ children }) {
+  return (
+    <div className="border solid border-3 p-3 border-blue-400 p-4 mb-2">
+      {children}
+    </div>
+  );
+}
